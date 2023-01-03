515b29be589564b67ad8cd1920ab600164214c08
natancalcagno <natanc25@gmail.com>
2023-01-02 21:51:37 -0300

    mdificação no urls.py do app receita

--- receita/urls.py
+++ receita/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from receita.views import home, contato, sobre
 
 urlpatterns = [
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', home, name='home'),
+    path('contato/', contato, name='contato'),
+    path('sobre/', sobre, name='sobre'),
 ]
