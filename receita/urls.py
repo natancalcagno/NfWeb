60e1c9a8b3d61d1a27308091d17d634d49663c26
natancalcagno <natanc25@gmail.com>
2023-01-31 18:23:19 -0300

    criando links no recipe.html, name no urls.py

--- receita/urls.py
+++ receita/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('receita/<int:id>/', views.recipe),
+    path('', views.home, name="receita-home"),
+    path('receita/<int:id>/', views.recipe, name="receitas-receita"),
     
 ]
